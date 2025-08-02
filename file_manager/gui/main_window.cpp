2c73bddd937ba25ac03f32441a1f0438040d490f
Abhiraj Patel <abhi0808raj@gmail.com>
2025-08-02 16:44:43 +0530

    Updates Test GUI

--- file_manager/gui/main_window.cpp
+++ file_manager/gui/main_window.cpp
@@ -1,49 +1,122 @@
 #include "gui/main_window.hpp"
-#include <QMenuBar>
-#include <QToolBar>
-#include <QStatusBar>
-#include <QFileSystemModel>
-#include <QTreeView>
-#include <QSplitter>
-#include <QLabel>
+#include "ui_mainwindow.h" // The header file generated from mainwindow.ui
+
+#include <QTreeWidgetItem>
+#include <QDir>
+#include <QDesktopServices>
+#include <QUrl>
 
 MainWindow::MainWindow(QWidget* parent)
-    : QMainWindow(parent)
+    : QMainWindow(parent),
+      ui(new Ui::MainWindow)
 {
-    setupUi();
+    ui->setupUi(this);
+
+    // Note on Icons: The .ui file does not include icons. For a complete look,
+    // you would create a Qt Resource file (.qrc), add your icon files to it,
+    // and then set them in the designer or in code like this:
+    // ui->actionBack->setIcon(QIcon(":/icons/arrow_back.png"));
+
+    setupModels();
+    setupConnections();
+
+    // Set initial path to Home
+    navigateToPath(QDir::homePath());
 }
 
-MainWindow::~MainWindow() = default;
-
-void MainWindow::setupUi() {
-    setWindowTitle("C++ Qt File Manager");
-
-    // Central widget setup
-    auto* treeView = new QTreeView(this);
-    auto* model = new QFileSystemModel(this);
-    model->setRootPath(QDir::rootPath());
-    treeView->setModel(model);
-    treeView->setRootIndex(model->index(QDir::homePath()));
-
-    setCentralWidget(treeView);
-
-    setupMenuBar();
-    setupToolBar();
-    setupStatusBar();
+MainWindow::~MainWindow()
+{
+    delete ui;
 }
 
-void MainWindow::setupMenuBar() {
-    auto* fileMenu = menuBar()->addMenu("File");
-    fileMenu->addAction("Exit", this, &MainWindow::close);
+void MainWindow::setupModels() {
+    m_fileSystemModel = new QFileSystemModel(this);
+    m_fileSystemModel->setRootPath(""); // Allow viewing the whole filesystem
+    m_fileSystemModel->setFilter(QDir::AllEntries | QDir::NoDotAndDotDot);
+
+    ui->fileTableView->setModel(m_fileSystemModel);
+
+    // Set table view properties
+    ui->fileTableView->setSelectionBehavior(QAbstractItemView::SelectRows);
+    ui->fileTableView->setSortingEnabled(true);
+    ui->fileTableView->sortByColumn(0, Qt::AscendingOrder);
 }
 
-void MainWindow::setupToolBar() {
-    auto* toolbar = addToolBar("Main Toolbar");
-    toolbar->addAction("Home", [this]() {
-        // Go to home directory (update root index)
-    });
+void MainWindow::setupConnections() {
+    // These connections are for the custom slots defined in the header
+    connect(ui->actionBack, &QAction::triggered, this, &MainWindow::on_actionBack_triggered);
+    connect(ui->actionForward, &QAction::triggered, this, &MainWindow::on_actionForward_triggered);
+    connect(ui->actionUp, &QAction::triggered, this, &MainWindow::on_actionUp_triggered);
+
+    connect(ui->fileTableView, &QTableView::doubleClicked, this, &MainWindow::on_fileTableView_doubleClicked);
+    connect(ui->navigationTreeWidget, &QTreeWidget::itemClicked, this, &MainWindow::on_navigationTreeWidget_itemClicked);
 }
 
-void MainWindow::setupStatusBar() {
-    statusBar()->showMessage("Ready");
+void MainWindow::navigateToPath(const QString& path) {
+    if (path.isEmpty()) return;
+
+    QDir dir(path);
+    if (!dir.exists()) return;
+
+    ui->fileTableView->setRootIndex(m_fileSystemModel->index(path));
+    setWindowTitle(path);
+
+    // Add to history
+    if (m_historyIndex < 0 || m_history[m_historyIndex] != path) {
+        if (m_historyIndex < m_history.size() - 1) {
+            m_history.erase(m_history.begin() + m_historyIndex + 1, m_history.end());
+        }
+        m_history.append(path);
+        m_historyIndex++;
+    }
+
+    ui->actionBack->setEnabled(m_historyIndex > 0);
+    ui->actionForward->setEnabled(m_historyIndex < m_history.size() - 1);
+}
+
+void MainWindow::on_fileTableView_doubleClicked(const QModelIndex &index) {
+    const QString path = m_fileSystemModel->filePath(index);
+    if (m_fileSystemModel->isDir(index)) {
+        navigateToPath(path);
+    } else {
+        QDesktopServices::openUrl(QUrl::fromLocalFile(path));
+    }
+}
+
+void MainWindow::on_navigationTreeWidget_itemClicked(QTreeWidgetItem *item, int column) {
+    QString location = item->text(column);
+    QString path;
+
+    if (location == "Home") path = QDir::homePath();
+    else if (location == "Desktop") path = QDir::homePath() + "/Desktop";
+    else if (location == "Downloads") path = QDir::homePath() + "/Downloads";
+    else if (location == "Filesystem Root") path = "/";
+    // Add other favorite locations here
+
+    if (!path.isEmpty()) {
+        navigateToPath(path);
+    }
+}
+
+void MainWindow::on_actionUp_triggered() {
+    QModelIndex currentRootIndex = ui->fileTableView->rootIndex();
+    QString currentPath = m_fileSystemModel->filePath(currentRootIndex);
+    QDir dir(currentPath);
+    if (dir.cdUp()) {
+        navigateToPath(dir.path());
+    }
+}
+
+void MainWindow::on_actionBack_triggered() {
+    if (m_historyIndex > 0) {
+        m_historyIndex--;
+        navigateToPath(m_history[m_historyIndex]);
+    }
+}
+
+void MainWindow::on_actionForward_triggered() {
+    if (m_historyIndex < m_history.size() - 1) {
+        m_historyIndex++;
+        navigateToPath(m_history[m_historyIndex]);
+    }
 }
--- include/gui/main_window.hpp
+++ include/gui/main_window.hpp
@@ -1,6 +1,13 @@
 #pragma once
 
 #include <QMainWindow>
+#include <QFileSystemModel>
+#include <QTreeWidgetItem>
+
+// Forward declaration of the auto-generated UI class
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow {
     Q_OBJECT
@@ -9,9 +16,20 @@ public:
     MainWindow(QWidget* parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_fileTableView_doubleClicked(const QModelIndex &index);
+    void on_navigationTreeWidget_itemClicked(QTreeWidgetItem *item, int column);
+    void on_actionUp_triggered();
+    void on_actionBack_triggered();
+    void on_actionForward_triggered();
+
 private:
-    void setupUi();         // setup widgets/layouts
-    void setupMenuBar();    // file/edit/view/help menus
-    void setupToolBar();    // navigation buttons
-    void setupStatusBar();  // current path, messages
+    void setupModels();
+    void setupConnections();
+    void navigateToPath(const QString& path);
+
+    Ui::MainWindow *ui;
+    QFileSystemModel *m_fileSystemModel;
+    QList<QString> m_history;
+    int m_historyIndex = -1;
 };
